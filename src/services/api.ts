b1290f34bb5ce15b4d07b76ca903596b60ac25ce
gokulrajrz <rajgokul9847161393@gmail.com>
2025-08-20 00:45:28 +0530

    Revert "Updated api.ts"
    This reverts commit e4829e73c03027ed09bf3939c374a64c942e09a0.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-const API_BASE_URL = 'https://datadump.rareminds.in/api'
-  // process.env.NODE_ENV === 'production'
-  //   ? 'https://datadump.rareminds.in/api'
-  //   : 'http://localhost:3001';
+const API_BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://datadump.rareminds.in/api'
+    : 'http://localhost:3001';
 
 export interface Project {
   id: string;
